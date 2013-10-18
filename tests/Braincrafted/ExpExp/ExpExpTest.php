6740427f9e7694817c8b4ec60a8f8cdd263eceaf
Florian Eckerstorfer <florian@theroadtojoy.at>
2013-10-18 18:12:51 +0200

    Improved tests

--- tests/Braincrafted/ExpExp/ExpExpTest.php
+++ tests/Braincrafted/ExpExp/ExpExpTest.php
@@ -34,248 +34,62 @@ class ExpExpTest extends \PHPUnit_Framework_TestCase
 
     /**
      * @covers Braincrafted\ExpExp\ExpExp::expand()
-     * @covers Braincrafted\ExpExp\ExpExp::addToAll()
-     * @covers Braincrafted\ExpExp\ExpExp::charAt()
-     * @covers Braincrafted\ExpExp\ExpExp::addChar()
+     * @dataProvider expandProvider
      */
-    public function testExpandSimpleDisjunction()
+    public function testExpand($pattern, $expectedCount, array $elements)
     {
-    	$result = $this->exp->expand('[abc]xyz[abc]');
+        $result = $this->exp->expand($pattern);
 
-    	$this->assertCount(9, $result);
-    	$this->assertContains('axyza', $result);
-    	$this->assertContains('axyzb', $result);
-    	$this->assertContains('axyzc', $result);
-    	$this->assertContains('bxyza', $result);
-    	$this->assertContains('bxyzb', $result);
-    	$this->assertContains('bxyzc', $result);
-    	$this->assertContains('cxyza', $result);
-    	$this->assertContains('cxyzb', $result);
-    	$this->assertContains('cxyzc', $result);
+        $this->assertCount($expectedCount, $result);
+        foreach ($elements as $element) {
+            $this->assertContains($element, $result);
+        }
     }
 
-    /**
-     * @covers Braincrafted\ExpExp\ExpExp::expand()
-     * @covers Braincrafted\ExpExp\ExpExp::addToAll()
-     * @covers Braincrafted\ExpExp\ExpExp::charAt()
-     * @covers Braincrafted\ExpExp\ExpExp::addChar()
-     */
-    public function testExpandEscapedDisjunction()
+    public function expandProvider()
     {
-    	$result = $this->exp->expand('\[abc\]xyz\[abc\]');
-    	$this->assertCount(1, $result);
-    	$this->assertContains('[abc]xyz[abc]', $result);
-    }
-
-    public function testExpandMultiplication()
-    {
-        $result = $this->exp->expand('a{3}');
-        $this->assertCount(1, $result);
-        $this->assertContains('aaa', $result);
-    }
-
-    public function testExpandEmptyMultiplication()
-    {
-        $result = $this->exp->expand('a{}');
-        $this->assertCount(1, $result);
-        $this->assertContains('a', $result);
-    }
-
-    public function testExpandMinMaxMultiplication()
-    {
-        $result = $this->exp->expand('a{1,3}');
-        $this->assertCount(3, $result);
-        $this->assertContains('a', $result);
-        $this->assertContains('aa', $result);
-        $this->assertContains('aaa', $result);
-    }
-
-    public function testExpandMaxMultiplication()
-    {
-        $result = $this->exp->expand('a{,3}');
-        $this->assertCount(4, $result);
-        $this->assertContains('', $result);
-        $this->assertContains('a', $result);
-        $this->assertContains('aa', $result);
-        $this->assertContains('aaa', $result);
-    }
-
-    public function testExpandParanthesesMultiplication()
-    {
-        $result = $this->exp->expand('a(bc){2}');
-        $this->assertCount(1, $result);
-        $this->assertContains('abcbc', $result);
-    }
-
-    public function testExpandParanthesesMinMaxMultiplication()
-    {
-        $result = $this->exp->expand('a(bc){1,2}');
-        $this->assertCount(2, $result);
-        $this->assertContains('abc', $result);
-        $this->assertContains('abcbc', $result);
-    }
-
-
-    public function testExpandParanthesesMaxMultiplication()
-    {
-        $result = $this->exp->expand('a(bc){,2}');
-        $this->assertCount(3, $result);
-        $this->assertContains('a', $result);
-        $this->assertContains('abc', $result);
-        $this->assertContains('abcbc', $result);
-    }
-
-    public function testExpandDisjunctionMutliplication()
-    {
-        $result = $this->exp->expand('[ab]{2}');
-        $this->assertCount(2, $result);
-        $this->assertContains('aa', $result);
-        $this->assertContains('bb', $result);
-    }
-
-    public function testExpandDisjunctionMinMaxMutliplication()
-    {
-        $result = $this->exp->expand('[ab]{0,2}');
-        $this->assertCount(6, $result);
-        $this->assertContains('', $result);
-        $this->assertContains('a', $result);
-        $this->assertContains('b', $result);
-        $this->assertContains('aa', $result);
-        $this->assertContains('bb', $result);
-    }
-
-    /**
-     * @covers Braincrafted\ExpExp\ExpExp::expand()
-     * @covers Braincrafted\ExpExp\ExpExp::charAt()
-     * @covers Braincrafted\ExpExp\ExpExp::addChar()
-     */
-    public function testEscapedEscaped()
-    {
-       	$result = $this->exp->expand('\\\abc');
-    	$this->assertCount(1, $result);
-    	$this->assertContains('\abc', $result);
-    }
-
-    /**
-     * @covers Braincrafted\ExpExp\ExpExp::expand()
-     * @covers Braincrafted\ExpExp\ExpExp::addToAll()
-     * @covers Braincrafted\ExpExp\ExpExp::charAt()
-     * @covers Braincrafted\ExpExp\ExpExp::addChar()
-     */
-    public function testExpandDotOperator()
-    {
-    	$result = $this->exp->expand('ab.');
-    	$this->assertCount(63, $result);
-    	$this->assertContains('abA', $result);
-    	$this->assertContains('aba', $result);
-    	$this->assertContains('ab0', $result);
-    	$this->assertContains('ab-', $result);
-    }
-
-    /**
-     * @covers Braincrafted\ExpExp\ExpExp::expand()
-     * @covers Braincrafted\ExpExp\ExpExp::charAt()
-     * @covers Braincrafted\ExpExp\ExpExp::addChar()
-     */
-    public function testExpandEscapedDotOperator()
-    {
-    	$result = $this->exp->expand('ab\.');
-    	$this->assertCount(1, $result);
-    	$this->assertContains('ab.', $result);
-    }
-
-    /**
-     * @covers Braincrafted\ExpExp\ExpExp::expand()
-     * @covers Braincrafted\ExpExp\ExpExp::addToAll()
-     * @covers Braincrafted\ExpExp\ExpExp::charAt()
-     * @covers Braincrafted\ExpExp\ExpExp::addChar()
-     */
-	public function testExpandParantheses()
-	{
-		$result = $this->exp->expand('ab(c)');
-		$this->assertCount(1, $result);
-		$this->assertContains('abc', $result);
-	}
-
-    /**
-     * @covers Braincrafted\ExpExp\ExpExp::expand()
-     * @covers Braincrafted\ExpExp\ExpExp::charAt()
-     * @covers Braincrafted\ExpExp\ExpExp::addChar()
-     */
-    public function testExpandAlternation()
-    {
-		$result = $this->exp->expand('abc|xyz');
-		$this->assertCount(2, $result);
-		$this->assertContains('abc', $result);
-		$this->assertContains('xyz', $result);
-    }
-
-    /**
-     * @covers Braincrafted\ExpExp\ExpExp::expand()
-     * @covers Braincrafted\ExpExp\ExpExp::addToAll()
-     * @covers Braincrafted\ExpExp\ExpExp::charAt()
-     * @covers Braincrafted\ExpExp\ExpExp::addChar()
-     */
-    public function testExpandAlternationInParantheses()
-    {
-		$result = $this->exp->expand('ab(c|d)');
-		$this->assertCount(2, $result);
-		$this->assertContains('abc', $result);
-		$this->assertContains('abd', $result);
-    }
-
-    /**
-     * @covers Braincrafted\ExpExp\ExpExp::expand()
-     * @covers Braincrafted\ExpExp\ExpExp::addToAll()
-     * @covers Braincrafted\ExpExp\ExpExp::charAt()
-     * @covers Braincrafted\ExpExp\ExpExp::addChar()
-     */
-    public function testExpandAlternationDisjunctionInParantheses()
-    {
-		$result = $this->exp->expand('ab(cde|[xyz])');
-		$this->assertCount(4, $result);
-		$this->assertContains('abcde', $result);
-		$this->assertContains('abx', $result);
-		$this->assertContains('aby', $result);
-		$this->assertContains('abz', $result);
-    }
-
-    /**
-     * @covers Braincrafted\ExpExp\ExpExp::expand()
-     * @covers Braincrafted\ExpExp\ExpExp::charAt()
-     * @covers Braincrafted\ExpExp\ExpExp::addChar()
-     */
-    public function testExpandOptional()
-    {
-    	$result = $this->exp->expand('abc?');
-    	$this->assertCount(2, $result);
-    	$this->assertContains('abc', $result);
-    	$this->assertContains('ab', $result);
-    }
-
-    /**
-     * @covers Braincrafted\ExpExp\ExpExp::expand()
-     * @covers Braincrafted\ExpExp\ExpExp::addToAll()
-     * @covers Braincrafted\ExpExp\ExpExp::charAt()
-     * @covers Braincrafted\ExpExp\ExpExp::addChar()
-     */
-    public function testExpandOptionalParantheses()
-    {
-    	$result = $this->exp->expand('abc(xyz)?');
-    	$this->assertCount(2, $result);
-    	$this->assertContains('abc', $result);
-    	$this->assertContains('abcxyz', $result);
-    }
-
-    /**
-     * @covers Braincrafted\ExpExp\ExpExp::expand()
-     * @covers Braincrafted\ExpExp\ExpExp::charAt()
-     * @covers Braincrafted\ExpExp\ExpExp::addChar()
-     */
-    public function testExpandEscapedOptional()
-    {
-        $result = $this->exp->expand('abc\?');
-        $this->assertCount(1, $result);
-        $this->assertContains('abc?', $result);
+        return [
+            // Disjunction
+            [ '[abc]', 3, [ 'a', 'b', 'c' ] ],
+            [ '[abc]x[abc]', 9, [ 'axa', 'axb', 'axc', 'bxa', 'bxb', 'bxc', 'cxa', 'cxb', 'cxc' ] ],
+            // Repetition
+            [ 'a{3}', 1, [ 'aaa' ] ],
+            [ 'a{}', 1, [ 'a' ] ],
+            [ 'a{1,3}', 3, [ 'a', 'aa', 'aaa' ] ],
+            [ 'a{,3}', 4, [ '', 'a', 'aa', 'aaa' ] ],
+            // Parentheses
+            [ 'ab(c)', 1, [ 'abc' ] ],
+            // [ 'a(b(c))', 1, [ 'abc' ] ],
+            // Parentheses + repetition
+            [ 'a(bc){2}', 1, [ 'abcbc' ] ],
+            [ 'a(bc){1,2}', 2, [ 'abc', 'abcbc' ] ],
+            [ 'a(bc){,2}', 3, [ 'a', 'abc', 'abcbc' ] ],
+            // Disjunction + repetition
+            [ '[ab]{2}', 2, [ 'aa', 'bb' ] ],
+            [ '[ab]{0,2}', 6, [ '', 'a', 'aa', 'b', 'bb' ] ],
+            // Dot operator
+            [ 'ab.', 63, [ 'abA', 'abB', 'aba', 'ab0', 'ab-' ] ],
+            // Alternation
+            [ 'abc|xyz', 2, [ 'abc', 'xyz' ] ],
+            // Alternation in parentheses
+            [ 'ab(c|d)', 2, [ 'abc', 'abd' ] ],
+            // [ 'a(b|(c|d))', 3, [ 'ab', 'ac', 'ad' ] ],
+            // Alternation + disjunction in parentheses
+            [ 'ab(cde|[xyz])', 4, [ 'abcde', 'abx', 'aby', 'abz' ] ],
+            // Optional
+            [ 'abc?', 2, [ 'abc', 'ab' ] ],
+            [ 'abc(xyz)?', 2, [ 'abc', 'abcxyz' ] ],
+            // Escaped control characters
+            [ '\[abc\]x', 1, [ '[abc]x' ] ],
+            [ '\[abc]x', 1, [ '[abc]x' ] ],
+            [ 'a\{3\}', 1, [ 'a{3}' ] ],
+            [ 'a\{3}', 1, [ 'a{3}' ] ],
+            [ 'abc\?', 1, [ 'abc?' ] ],
+            [ 'ab\.', 1, [ 'ab.' ] ],
+            [ 'a\|b', 1, [ 'a|b' ] ],
+            [ '\(a\)', 1, [ '(a)' ] ],
+            [ '\(a)', 1, [ '(a)' ] ],
+            [ '\\\abc', 1, [ '\abc' ] ]
+        ];
     }
 }
