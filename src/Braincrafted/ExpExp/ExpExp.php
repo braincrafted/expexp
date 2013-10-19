812036cad2cdbd4b6ae3f6b1d23cecf2cb62aa2c
Florian Eckerstorfer <florian@theroadtojoy.at>
2013-10-19 21:54:37 +0200

    Fixed indentetion

--- src/Braincrafted/ExpExp/ExpExp.php
+++ src/Braincrafted/ExpExp/ExpExp.php
@@ -25,241 +25,241 @@ class ExpExp
 
     /** @var array */
     private $classes = [
-        'digit'	=> '0123456789',
-		'lower' => 'abcdefghijklmnopqrstuvwxyz',
-		'upper'	=> 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
-		'word'  => '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_',
-		'space' => "\t\n\r ",
-		'vspace' => "\n\r",
-		'hspace' => "\t "
-	];
+        'digit'    => '0123456789',
+        'lower' => 'abcdefghijklmnopqrstuvwxyz',
+        'upper'    => 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+        'word'  => '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_',
+        'space' => "\t\n\r ",
+        'vspace' => "\n\r",
+        'hspace' => "\t "
+    ];
 
-	/** @var integer */
-	private $pos;
+    /** @var integer */
+    private $pos;
 
-	/** @var array */
-	private $result;
+    /** @var array */
+    private $result;
 
-	/**
-	 * Expands the pattern.
-	 *
-	 * @param string $pattern  Pattern
-	 * @param string $stopChar Character on which the expansion should stop
-	 *
-	 * @return array Expanded pattern
-	 */
-	public function expand($pattern, $stopChar = null)
-	{
-		$this->pos = 0;
-		$this->result = [ '' ];
-		$escape = false;
-		$alternateResults = [ ];
+    /**
+     * Expands the pattern.
+     *
+     * @param string $pattern  Pattern
+     * @param string $stopChar Character on which the expansion should stop
+     *
+     * @return array Expanded pattern
+     */
+    public function expand($pattern, $stopChar = null)
+    {
+        $this->pos = 0;
+        $this->result = [ '' ];
+        $escape = false;
+        $alternateResults = [ ];
 
-		while ($this->pos < strlen($pattern)) {
-			$char = substr($pattern, $this->pos, 1);
-			$nextChar = substr($pattern, $this->pos+1, 1);
+        while ($this->pos < strlen($pattern)) {
+            $char = substr($pattern, $this->pos, 1);
+            $nextChar = substr($pattern, $this->pos+1, 1);
 
-			if (null !== $stopChar && $char === $stopChar) {
-				$this->pos += 1;
-				break;
-			}
+            if (null !== $stopChar && $char === $stopChar) {
+                $this->pos += 1;
+                break;
+            }
 
-			if (false === $escape && '\\' === $char) {
-				$escape = $this->pos;
-			} else if (false === $escape && '{' === $nextChar) {
-				$this->pos += 1;
-				$buffer = $this->repeat($pattern, $char);
-				$this->addAll($buffer);
-			} else if (false === $escape && '(' === $char) {
-				$bufferExp = new ExpExp;
-				$charBuffer = $bufferExp->expand(substr($pattern, $this->pos+1), ')');
-				$this->pos += $bufferExp->getPos();
-				if ('?' === substr($pattern, $this->pos+1, 1)) {
-					$this->pos += 1;
-					$charBuffer[] = '';
-				} else if ('{' === substr($pattern, $this->pos+1, 1)) {
-					$this->pos += 1;
-					$charBuffer = $this->repeat($pattern, $charBuffer);
-				}
-				$this->addAll($charBuffer);
-			} else if (false === $escape && '[' === $char && ':' !== $nextChar) {
-				$bufferExp = new ExpExp;
-				$buffer = $bufferExp->expand(substr($pattern, $this->pos+1), ']');
-				$buffer[0] = preg_replace_callback(
-					'/(\[:([a-z]+):\])/',
-					function ($matches) {
-						return $this->getClass($matches[2]);
-					},
-					$buffer[0]
-				);
-				$buffer = str_split($buffer[0], 1);
-				$this->pos += $bufferExp->getPos();
-				if ('{' === substr($pattern, $this->pos+1, 1)) {
-					$this->pos += 1;
-					$buffer = $this->repeat($pattern, $buffer);
-				}
-				$this->addAll($buffer);
-			} else if (false === $escape && '?' === $nextChar) {
-				$this->pos += 1;
-				$this->addAll([ $char, '' ]);
-			} else if (false === $escape && '.' === $char) {
-				$this->addAll(str_split($this->dotChars, 1));
-			} else if (false === $escape && '|' === $char) {
-				$alternateResults[] = $this->result;
-				$this->result = [ '' ];
-			} else if (false !== $escape && 'd' === $char) {
-				$this->addAll(str_split($this->getClass('digit'), 1));
-			} else if (false !== $escape && 'w' === $char) {
-				$this->addAll(str_split($this->getClass('word'), 1));
-			} else if (false !== $escape && 's' === $char) {
-				$this->addAll(str_split($this->getClass('space'), 1));
-			} else if (false !== $escape && 'v' === $char) {
-				$this->addAll(str_split($this->getClass('vspace'), 1));
-			} else if (false !== $escape && 'h' === $char) {
-				$this->addAll(str_split($this->getClass('hspace'), 1));
-			} else if (false === $escape && ':' === $char && ']' == $nextChar) {
-				$this->pos += 1;
-				$this->add(':]');
-			} else {
-				$this->add($char);
-			}
+            if (false === $escape && '\\' === $char) {
+                $escape = $this->pos;
+            } else if (false === $escape && '{' === $nextChar) {
+                $this->pos += 1;
+                $buffer = $this->repeat($pattern, $char);
+                $this->addAll($buffer);
+            } else if (false === $escape && '(' === $char) {
+                $bufferExp = new ExpExp;
+                $charBuffer = $bufferExp->expand(substr($pattern, $this->pos+1), ')');
+                $this->pos += $bufferExp->getPos();
+                if ('?' === substr($pattern, $this->pos+1, 1)) {
+                    $this->pos += 1;
+                    $charBuffer[] = '';
+                } else if ('{' === substr($pattern, $this->pos+1, 1)) {
+                    $this->pos += 1;
+                    $charBuffer = $this->repeat($pattern, $charBuffer);
+                }
+                $this->addAll($charBuffer);
+            } else if (false === $escape && '[' === $char && ':' !== $nextChar) {
+                $bufferExp = new ExpExp;
+                $buffer = $bufferExp->expand(substr($pattern, $this->pos+1), ']');
+                $buffer[0] = preg_replace_callback(
+                    '/(\[:([a-z]+):\])/',
+                    function ($matches) {
+                        return $this->getClass($matches[2]);
+                    },
+                    $buffer[0]
+                );
+                $buffer = str_split($buffer[0], 1);
+                $this->pos += $bufferExp->getPos();
+                if ('{' === substr($pattern, $this->pos+1, 1)) {
+                    $this->pos += 1;
+                    $buffer = $this->repeat($pattern, $buffer);
+                }
+                $this->addAll($buffer);
+            } else if (false === $escape && '?' === $nextChar) {
+                $this->pos += 1;
+                $this->addAll([ $char, '' ]);
+            } else if (false === $escape && '.' === $char) {
+                $this->addAll(str_split($this->dotChars, 1));
+            } else if (false === $escape && '|' === $char) {
+                $alternateResults[] = $this->result;
+                $this->result = [ '' ];
+            } else if (false !== $escape && 'd' === $char) {
+                $this->addAll(str_split($this->getClass('digit'), 1));
+            } else if (false !== $escape && 'w' === $char) {
+                $this->addAll(str_split($this->getClass('word'), 1));
+            } else if (false !== $escape && 's' === $char) {
+                $this->addAll(str_split($this->getClass('space'), 1));
+            } else if (false !== $escape && 'v' === $char) {
+                $this->addAll(str_split($this->getClass('vspace'), 1));
+            } else if (false !== $escape && 'h' === $char) {
+                $this->addAll(str_split($this->getClass('hspace'), 1));
+            } else if (false === $escape && ':' === $char && ']' == $nextChar) {
+                $this->pos += 1;
+                $this->add(':]');
+            } else {
+                $this->add($char);
+            }
 
-			if ($this->pos > $escape) {
-				$escape = false;
-			}
-			$this->pos += 1;
-		}
+            if ($this->pos > $escape) {
+                $escape = false;
+            }
+            $this->pos += 1;
+        }
 
-		$this->mergeResults($alternateResults);
+        $this->mergeResults($alternateResults);
 
-		return $this->result;
-	}
+        return $this->result;
+    }
 
-	/**
-	 * Returns the current position of the expansion
-	 *
-	 * @return integer Current position of the expansion
-	 */
-	public function getPos()
-	{
-	    return $this->pos;
-	}
+    /**
+     * Returns the current position of the expansion
+     *
+     * @return integer Current position of the expansion
+     */
+    public function getPos()
+    {
+        return $this->pos;
+    }
 
-	/**
-	 * Merges the results from the given alternatives with the result.
-	 *
-	 * @param array $alternates List of alternate results
-	 */
-	protected function mergeResults($alternates)
-	{
-		$buffer = [];
+    /**
+     * Merges the results from the given alternatives with the result.
+     *
+     * @param array $alternates List of alternate results
+     */
+    protected function mergeResults($alternates)
+    {
+        $buffer = [];
 
-	    foreach ($alternates as $alternate) {
-	    	$buffer = array_merge($buffer, $alternate);
-	    }
+        foreach ($alternates as $alternate) {
+            $buffer = array_merge($buffer, $alternate);
+        }
 
-	    $this->result = array_merge($buffer, $this->result);
-	}
+        $this->result = array_merge($buffer, $this->result);
+    }
 
-	/**
-	 * Adds the given string to every element in the result.
-	 *
-	 * @param string $string String to add to the result
-	 */
-	protected function add($string)
-	{
-		$buffer = [];
+    /**
+     * Adds the given string to every element in the result.
+     *
+     * @param string $string String to add to the result
+     */
+    protected function add($string)
+    {
+        $buffer = [];
 
-	    for ($i = 0; $i < count($this->result); $i++) {
-	    	$buffer[] = $this->result[$i].$string;
-	    }
+        for ($i = 0; $i < count($this->result); $i++) {
+            $buffer[] = $this->result[$i].$string;
+        }
 
-	    $this->result = $buffer;
-	}
+        $this->result = $buffer;
+    }
 
-	/**
-	 * Adds all given strings to every element in the result.
-	 *
-	 * @param array $strings Array of strings to add to the result
-	 */
-	protected function addAll(array $strings)
-	{
-	    $buffer = [];
+    /**
+     * Adds all given strings to every element in the result.
+     *
+     * @param array $strings Array of strings to add to the result
+     */
+    protected function addAll(array $strings)
+    {
+        $buffer = [];
 
-	    for ($i = 0; $i < count($this->result); $i++) {
-	    	for ($j = 0; $j < count($strings); $j++) {
-	    		$buffer[] = $this->result[$i].$strings[$j];
-	    	}
-	    }
+        for ($i = 0; $i < count($this->result); $i++) {
+            for ($j = 0; $j < count($strings); $j++) {
+                $buffer[] = $this->result[$i].$strings[$j];
+            }
+        }
 
-	    $this->result = $buffer;
-	}
+        $this->result = $buffer;
+    }
 
-	/**
-	 * Repeats the given string depending on the given pattern.
-	 *
-	 * @param string $pattern Pattern
-	 * @param string $add     String to repeat
-	 *
-	 * @return array Result of the repetition
-	 */
-	protected function repeat($pattern, $add)
-	{
-		if (false === is_array($add)) {
-			$add = [ $add ];
-		}
+    /**
+     * Repeats the given string depending on the given pattern.
+     *
+     * @param string $pattern Pattern
+     * @param string $add     String to repeat
+     *
+     * @return array Result of the repetition
+     */
+    protected function repeat($pattern, $add)
+    {
+        if (false === is_array($add)) {
+            $add = [ $add ];
+        }
 
-		$bufferExp = new ExpExp;
-		$buffer = $bufferExp->expand(substr($pattern, $this->pos+1), '}')[0];
-		list($min, $max) = $this->parseRepetition($buffer);
+        $bufferExp = new ExpExp;
+        $buffer = $bufferExp->expand(substr($pattern, $this->pos+1), '}')[0];
+        list($min, $max) = $this->parseRepetition($buffer);
 
-		$this->pos += $bufferExp->getPos();
+        $this->pos += $bufferExp->getPos();
 
-		$buffer = [];
-		for ($i = $min; $i <= $max; $i++) {
-			foreach ($add as $el) {
-				$buffer[] = str_repeat($el, $i);
-			}
-		}
+        $buffer = [];
+        for ($i = $min; $i <= $max; $i++) {
+            foreach ($add as $el) {
+                $buffer[] = str_repeat($el, $i);
+            }
+        }
 
-		return $buffer;
-	}
+        return $buffer;
+    }
 
-	/**
-	 * Parses the min and max values from the given repetition pattern.
-	 *
-	 * @param string $string Repetition pattern
-	 *
-	 * @return array Array with two values, min and max
-	 */
-	protected function parseRepetition($string)
-	{
-		if (0 === strlen($string)) {
-			return [ 0, 0 ];
-		}
-	    if (false === strpos($string, ',')) {
-	    	return [ $string, $string ];
-	    }
+    /**
+     * Parses the min and max values from the given repetition pattern.
+     *
+     * @param string $string Repetition pattern
+     *
+     * @return array Array with two values, min and max
+     */
+    protected function parseRepetition($string)
+    {
+        if (0 === strlen($string)) {
+            return [ 0, 0 ];
+        }
+        if (false === strpos($string, ',')) {
+            return [ $string, $string ];
+        }
 
-	    list($min, $max) = explode(',', $string);
-	    if (!$min) {
-	    	$min = 0;
-	    }
+        list($min, $max) = explode(',', $string);
+        if (!$min) {
+            $min = 0;
+        }
 
-	    return [ $min, $max ];
-	}
+        return [ $min, $max ];
+    }
 
-	/**
-	 * Returns the character class(es) with the given name(s). Multiple classes can be connected by +.
-	 *
-	 * @param string $name Character class or list of character classes
-	 * @return string List of characters from the given class(es)
-	 */
-	protected function getClass($name)
-	{
-    	if (false === isset($this->classes[$name])) {
-	    	throw new \InvalidArgumentException(sprintf('The character class %s does not exist.', $name));
-	    }
-	    return $this->classes[$name];
-	}
+    /**
+     * Returns the character class(es) with the given name(s). Multiple classes can be connected by +.
+     *
+     * @param string $name Character class or list of character classes
+     * @return string List of characters from the given class(es)
+     */
+    protected function getClass($name)
+    {
+        if (false === isset($this->classes[$name])) {
+            throw new \InvalidArgumentException(sprintf('The character class %s does not exist.', $name));
+        }
+        return $this->classes[$name];
+    }
 }
